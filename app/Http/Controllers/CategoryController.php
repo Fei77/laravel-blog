6c2e7a698f2e90a70f0f24d7bc1b3b0211d7e93a
Fei77 <hifisaputra1@gmail.com>
2018-05-16 16:36:59 +0700

    added category show page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
@@ -17,68 +18,16 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        return view('pages.categories.show', [
+            'category' => Category::with('posts')->where('slug', $slug)->firstOrFail()
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
---
+++ resources/views/pages/categories/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', $category->name)
+
+@section('content')
+
+    @foreach($category->posts as $post)
+    <div class="single-blog">
+        <a href="{{ route('posts.show', $post->slug) }}"><h3 class="title">{{ $post->title or '' }}</h3></a>
+        <img src="{{ $post->image_url }}" alt="Blog Image" />
+        <p>{{ $post->excerpt or '' }}</p>
+        <div class="blog-info">
+            <ul>
+                <li><a href="{{ route('posts.search') }}">{{ $post->published_date or '' }}</a></li>
+                <li><a href="{{ route('posts.search') }}">{{ $post->author->name }}</a></li>
+                <li><a href="{{ route('categories.show', $post->category->slug) }}">{{ $post->category->name }}</a></li>
+                {{-- <li><a href="">10 Comments</a></li> --}}
+            </ul>
+            
+            <div class="read-more pull-right">
+                <a href="{{ route('posts.show', $post->slug) }}" class="btn btn-readmore">Continue Reading</a>
+            </div>
+            
+        </div>
+        
+    </div>
+    @endforeach
+    
+@endsection
